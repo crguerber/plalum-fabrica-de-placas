3a0c30d14fbc1a87a4c068fe4980559e21f20eb4
Carlos Rafael Guerber <crguerber@gmail.com>
2026-06-09 16:30:00 -0300

    Update Conexao.php

--- backend/src/Entity/Conexao.php
+++ backend/src/Entity/Conexao.php
@@ -22,7 +22,7 @@ class Conexao {
             $host = 'localhost';
             $dbname = 'plalum_db';
             $usuario = 'root';
-            $senha = 'core2duo@20153010'; 
+            $senha = 'suasenhaaqui'; 
             
             try {
                 // Criação da conexão utilizando PDO
